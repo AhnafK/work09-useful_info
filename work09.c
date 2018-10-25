17c047481fd6ccbab2e37232bacd15cdd0bfe8f3
AhnafK <akazi1@stuy.edu>
2018-10-24 23:20:19 -0400

    read file

--- work09.c
+++ work09.c
@@ -8,15 +8,20 @@
 int main(){
 
   //Path to the file.
-  char * address = "../text.txt";
+  char * address = "text.txt";
 
+  //buffer
+  char buf[27];
+  
   //Open the file with read and write flags.
   int fd = open(address,O_RDWR);
 
   //Reading the file
-  
+  read(fd,buf,26);
+  buf[27] = '\0';
+  printf("reading \"text.txt\": \n%s\n", buf);
   //Write into the file
-
+  
   //Read again to see changes
 
   //Close the file
